24a784510b9bb9bcbac6af264862a8e9ba57e94f
Mohammed Abu Halima <mohammed.abu.halima11@gmail.com>
2026-07-27 10:26:23 +0300

    Fixing some errors on the personal account page

--- assets/js/script.js
+++ assets/js/script.js
@@ -511,7 +511,7 @@ if (document.querySelector('header')) {
         document.querySelector('.background-with-sidbare').classList.add('hidden');
         document.body.style.overflow = 'auto';
 
-        
+
     });
 
 
@@ -522,8 +522,11 @@ if (document.querySelector('header')) {
         sidebare.classList.toggle('translate-x-full');
         document.querySelector('.background-with-sidbare').classList.toggle('hidden');
         document.body.style.overflow = 'hidden';
-        
-        if (!profile_menu_logout.classList.contains('hidden')) profile_menu_logout.classList.add('hidden');
+
+        if (!profile_menu_logout.classList.contains('hidden')) {
+            profile_menu_logout.classList.add('hidden');
+            profile_menu_logout_btn.classList.remove('-scale-y-100');
+        }
     }
 
     sidebare.onclick = (e) => {
@@ -756,7 +759,7 @@ if (customerDetailspage) {
         document.documentElement.style.overflow = '';
     }
 
-    
+
 }
 
 
@@ -1542,8 +1545,54 @@ if (document.querySelector(".date-picker")) {
         btn.addEventListener("click", (e) => {
             e.preventDefault();
             picker.open();
-            
+
         });
+
+    });
+}
+
+
+//======================================================================================//
+// password inputs
+
+if (document.querySelector('.password-input')) {
+    const passwordInputs = document.querySelectorAll('.password-input');
+    const showHideBtns = document.querySelectorAll('.show-hide-btn');
+
+    passwordInputs.forEach((input, index) => {
+
+        const btn = showHideBtns[index];
+
+        btn.onclick = () => {
+            if (input.type === 'password') {
+                input.type = 'text';
+                btn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none">
+<path d="M15.5799 12C15.5799 13.98 13.9799 15.58 11.9999 15.58C10.0199 15.58 8.41992 13.98 8.41992 12C8.41992 10.02 10.0199 8.41998 11.9999 8.41998C13.9799 8.41998 15.5799 10.02 15.5799 12Z" stroke="#86909C" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M11.9998 20.27C15.5298 20.27 18.8198 18.19 21.1098 14.59C22.0098 13.18 22.0098 10.81 21.1098 9.39997C18.8198 5.79997 15.5298 3.71997 11.9998 3.71997C8.46984 3.71997 5.17984 5.79997 2.88984 9.39997C1.98984 10.81 1.98984 13.18 2.88984 14.59C5.17984 18.19 8.46984 20.27 11.9998 20.27Z" stroke="#86909C" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>`;
+            }
+
+            else {
+                input.type = 'password';
+                btn.innerHTML = ` <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none">
+                            <path
+                                d="M14.5299 9.46998L9.46992 14.53C8.81992 13.88 8.41992 12.99 8.41992 12C8.41992 10.02 10.0199 8.41998 11.9999 8.41998C12.9899 8.41998 13.8799 8.81998 14.5299 9.46998Z"
+                                stroke="#86909C" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                            <path
+                                d="M17.8201 5.76998C16.0701 4.44998 14.0701 3.72998 12.0001 3.72998C8.47009 3.72998 5.18009 5.80998 2.89009 9.40998C1.99009 10.82 1.99009 13.19 2.89009 14.6C3.68009 15.84 4.60009 16.91 5.60009 17.77"
+                                stroke="#86909C" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                            <path
+                                d="M8.41992 19.53C9.55992 20.01 10.7699 20.27 11.9999 20.27C15.5299 20.27 18.8199 18.19 21.1099 14.59C22.0099 13.18 22.0099 10.81 21.1099 9.39999C20.7799 8.87999 20.4199 8.38999 20.0499 7.92999"
+                                stroke="#86909C" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M15.5099 12.7C15.2499 14.11 14.0999 15.26 12.6899 15.52" stroke="#86909C"
+                                stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M9.47 14.53L2 22" stroke="#86909C" stroke-width="1.5" stroke-linecap="round"
+                                stroke-linejoin="round" />
+                            <path d="M22 2L14.53 9.47" stroke="#86909C" stroke-width="1.5" stroke-linecap="round"
+                                stroke-linejoin="round" />
+                        </svg>`;
+            }
+        }
 
     });
 }
